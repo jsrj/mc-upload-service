18cce275334418bc4244ded89115448b7296688b
jsrj <rj.javadev@icloud.com>
2018-12-05 23:19:43 -0500

    added a userManager api-extension stub

---
+++ initializers/userManager.js
@@ -0,0 +1,29 @@
+'use strict'
+const ActionHero = require('actionhero')
+
+module.exports = class MyInitializer extends ActionHero.Initializer {
+  constructor () {
+    super()
+    this.name = 'userManager'
+    this.loadPriority = 1000
+    this.startPriority = 1000
+    this.stopPriority = 1000
+  }
+
+  async initialize () {
+    ActionHero.api['userManager'] = {
+
+      // constants
+      usersHash: "uploaderServiceUsers",
+
+      // methods
+      addUser: function(userName, password){},
+      listUsers: function(){},
+      authenticateUsers: function(userName, password){},
+      deleteUser: function(userName, password){},
+    }
+  }
+
+  async start () {}
+  async stop () {}
+}
