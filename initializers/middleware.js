923c9295f6e7c66e728f5241a10aa473b14b6a46
Arjay Jones <rj.javadev@icloud.com>
2018-12-08 00:11:20 -0500

    Changed classname from MyInitializer to Middleware

--- initializers/middleware.js
+++ initializers/middleware.js
@@ -1,7 +1,7 @@
 'use strict'
 const ActionHero = require('actionhero')
 
-module.exports = class MyInitializer extends ActionHero.Initializer {
+module.exports = class Middleware extends ActionHero.Initializer {
   constructor () {
     super()
     this.name = 'middleware'
