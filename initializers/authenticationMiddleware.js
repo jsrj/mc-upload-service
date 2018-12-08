b0d64b79c6c411208e622f395b2d66e34b9d973c
Arjay Jones <rj.javadev@icloud.com>
2018-12-08 00:42:03 -0500

    Added call to "authenticateUser()" method in the userManager initializer and added authentication middleware to api actions

--- initializers/authenticationMiddleware.js
+++ initializers/authenticationMiddleware.js
@@ -11,7 +11,18 @@ module.exports = class AuthenticationMiddleware extends ActionHero.Initializer {
   }
 
   async initialize () {
-    ActionHero.api['authenticationMiddleware'] = {}
+    const authMiddleware = {
+      name: this.name,
+      global: true,
+      preProcessor: async ({ actionTemplate, params }) => {
+        if (actionTemplate.authenticated === true) {
+          let match = await api.userManager.authenticateUser(params.userName, params.password)
+          if (!match) { throw Error('Authentication Failed.  userName and password required.') }
+        }
+      }
+    }
+
+    api.actions.addMiddleware(authMiddleware)
   }
 
   async start () {}
