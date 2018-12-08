a7686befc598246c5c11066a7c838916273251ba
Arjay Jones <rj.javadev@icloud.com>
2018-12-08 00:37:59 -0500

    changed middleware name to authenticationMiddleware

--- initializers/authenticationMiddleware.js
+++ initializers/authenticationMiddleware.js
@@ -1,17 +1,17 @@
 'use strict'
 const ActionHero = require('actionhero')
 
-module.exports = class Middleware extends ActionHero.Initializer {
+module.exports = class AuthenticationMiddleware extends ActionHero.Initializer {
   constructor () {
     super()
-    this.name = 'middleware'
+    this.name = 'authenticationMiddleware'
     this.loadPriority = 1000
     this.startPriority = 1000
     this.stopPriority = 1000
   }
 
   async initialize () {
-    ActionHero.api['middleware'] = {}
+    ActionHero.api['authenticationMiddleware'] = {}
   }
 
   async start () {}
