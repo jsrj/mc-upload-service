bf67e99d6587c452cb7de2987ffff77e2aea320e
Arjay Jones <rj.javadev@icloud.com>
2018-12-12 01:04:21 -0500

    Code reformatted to conform to standard

--- actions/s3Actions.js
+++ actions/s3Actions.js
@@ -14,13 +14,13 @@ module.exports = class Upload extends ActionHero.Action {
     }
   }
 
-  async run ({ response, params} ) {
+  async run ({ response, params }) {
     let authenticated = (!(params.token === undefined) && (params.token.length > 0)) // TODO: Actually code in validation for this once security is developed
     if (authenticated) { // Just placeholder functionality
-      await ActionHero.api.s3Uploader.upload(params.data);
-      response.message = "Image uploaded successfully"
+      await ActionHero.api.s3Uploader.upload(params.data)
+      response.message = 'Image uploaded successfully'
     } else {
-      throw new Error('You must log in to upload an image');
+      throw new Error('You must log in to upload an image')
     }
   }
 }
