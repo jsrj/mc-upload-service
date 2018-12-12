c370c85ff90e692ba9c89853330cb5f70e298979
Arjay Jones <rj.javadev@icloud.com>
2018-12-12 00:06:01 -0500

    Added stub to s3Actions for call to upload method in s3Uploader when a user is authenticated

--- actions/s3Actions.js
+++ actions/s3Actions.js
@@ -1,15 +1,26 @@
 'use strict'
 const ActionHero = require('actionhero')
 
-module.exports = class MyAction extends ActionHero.Action {
+module.exports = class Upload extends ActionHero.Action {
   constructor () {
     super()
-    this.name = 's3Actions'
-    this.description = 'an actionhero action'
+    this.name = 'uploadImage'
+    this.description = 'Upload An Image To S3'
     this.outputExample = {}
+    this.authenticated = true
+    this.inputs = {
+      data: { required: true },
+      token: { required: false }
+    }
   }
 
-  async run (data) {
-    // your logic here
+  async run ({ response, params} ) {
+    let authenticated = (!(params.token === undefined) && (params.token.length > 0)) // TODO: Actually code in validation for this once security is developed
+    if (authenticated) { // Just placeholder functionality
+      await ActionHero.api.s3Uploader.upload(params.data);
+      response.message = "Image uploaded successfully"
+    } else {
+      throw new Error('You must log in to upload an image');
+    }
   }
 }
